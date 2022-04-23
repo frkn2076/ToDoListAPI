6d18cf5c8e5eedf9017629d96cb56a17defb1693
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2022-04-23 15:23:21 +0300

    added filters for fluent validation

---
+++ src/Api/Filters/ValidateModelStateAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api.Filters;
+
+public class ValidateModelStateAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        if (!context.ModelState.IsValid)
+        {
+            var errors = context.ModelState.Values.Where(v => v.Errors.Count > 0)
+                    .SelectMany(v => v.Errors)
+                    .Select(v => v.ErrorMessage)
+                    .ToList();
+
+            throw new BadHttpRequestException(string.Join(Environment.NewLine, errors));
+        }
+    }
+}
