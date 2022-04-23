30066b71392d0913e7e952d49a549cd1708e1149
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2022-04-23 15:21:11 +0300

    mapster impl.

---
+++ src/Api/Mapper/Mappers.cs
@@ -0,0 +1,19 @@
+﻿using Api.Models.Requests;
+using Mapster;
+using Service.Models.Requests;
+
+namespace Api.Mapper;
+
+public class Mappers
+{
+    public static void Init()
+    {
+        TypeAdapterConfig<RegisterRequestModel, AuthenticationRequestDTO>.NewConfig();
+
+        //TypeAdapterConfig<TokenDTOResponse, BaseResponse>.NewConfig();
+        //.Map(dest => dest.AccessToken, src => src.AccessToken)
+        //.Map(dest => dest.AccessTokenExpiresIn, src => src.AccessTokenExpiresIn)
+        //.Map(dest => dest.RefreshToken, src => src.RefreshToken)
+        //.Map(dest => dest.RefreshTokenExpiresIn, src => src.RefreshTokenExpiresIn);
+    }
+}
