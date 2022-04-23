7142609432fc36553bce85de0988edabb7537fb3
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2022-04-23 15:19:59 +0300

    item service impl.

---
+++ src/Service/Implementations/ItemService.cs
@@ -0,0 +1,36 @@
+﻿using Data;
+using Data.Contracts;
+using Data.Entities;
+using Service.Contracts;
+using Service.Models;
+
+namespace Service.Implementations;
+
+public class ItemService : IItemService
+{
+    private readonly IRepository _repository;
+
+    public ItemService(IRepository repository)
+    {
+        _repository = repository;
+    }
+
+    //public async Task<ServiceResponse<IEnumerable<Activity>>> GetListsOfUserAsync(int profileId, int skip, int count)
+    //{
+    //    var activities = await _socialRepository.GetActivityAsync(count, skip);
+
+    //    if (activities == null || !activities.Any())
+    //    {
+    //        return new()
+    //        {
+    //            Error = ErrorMessages.NoRecordHasFound
+    //        };
+    //    }
+
+    //    return new()
+    //    {
+    //        IsSuccessful = true,
+    //        Response = activities
+    //    };
+    //}
+}
