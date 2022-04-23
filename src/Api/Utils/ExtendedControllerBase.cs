8a0e6c9607ed15aa431dc948ea4f60947dec60a7
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2022-04-23 22:08:41 +0300

    implementing service response handler for generic and non-generic responses

--- src/Api/Utils/ExtendedControllerBase.cs
+++ src/Api/Utils/ExtendedControllerBase.cs
@@ -6,13 +6,20 @@ namespace Api.Utils;
 
 public abstract class ExtendedControllerBase : ControllerBase
 {
-    public virtual IActionResult HandleServiceResponse<T>(ServiceResponse<T> serviceResponse)
+    public virtual IActionResult HandleServiceResponse(ServiceResponse serviceResponse)
     {
         if (!serviceResponse.IsSuccessful)
         {
             return StatusCode(500, ResponseModel.Failure(serviceResponse.Error));
         }
 
-        return Ok(ResponseModel.Success(serviceResponse.Response));
+        if (serviceResponse.GetType().IsGenericType)
+        {
+            var propertyInfo = serviceResponse.GetType().GetProperty("Response");
+            var response = propertyInfo?.GetValue(serviceResponse, null);
+            return Ok(ResponseModel.Success(response));
+        }
+
+        return Ok(ResponseModel.Success());
     }
 }
