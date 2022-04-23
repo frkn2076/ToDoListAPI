c2f12219d6793de9f77c8c7d6d494c2aaceb1d77
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2022-04-23 15:23:02 +0300

    created generic response model

---
+++ src/Api/Models/Responses/ResponseModel.cs
@@ -0,0 +1,32 @@
+﻿namespace Api.Models.Responses;
+
+public class ResponseModel
+{
+    public static ResponseModel Success(object response)
+    {
+        return new ResponseModel()
+        {
+            IsError = false,
+            ErrorMessage = null,
+            Response = response
+        };
+    }
+
+    public static ResponseModel Failure(string errorMessage)
+    {
+        return new ResponseModel()
+        {
+            IsError = true,
+            ErrorMessage = errorMessage,
+            Response = null
+        };
+    }
+
+    public bool IsError { get; set; }
+
+    public string ErrorMessage { get; set; }
+
+    public object Response { get; set; }
+}
+
+
